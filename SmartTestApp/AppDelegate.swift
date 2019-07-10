1ca918d2ccc6282708aa38bcdbf127921955bda9
Robert Brais <robrais@microsoft.com>
2019-07-09 23:21:27 -0700

    we have UIgit add .git add .

--- SmartTestApp/AppDelegate.swift
+++ SmartTestApp/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        window?.makeKeyAndVisible()
         // authorize, then search for prescriptions
         smart.authorize() { patient, error in
             if nil != error || nil == patient {
